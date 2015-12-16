16e7a529a5359ab8ac9c23c51434835b35114e4e
Rye Skelton <rye@musubi00.local>
2015-12-16 13:30:34 -0800

    simple search example

--- spider.py
+++ spider.py
@@ -1,46 +1,33 @@
-from html.parser import HTMLparser
-from urllib.request import urlopen
-from urllib import parse
+import mechanize
+import urllib
+import json
+from bs4 import BeautifulSoup
 
-class LinkParser(HTMLparser): 
-	def handle_starttag(self, tag, attrs):
-		if tag == 'a':
-			for (key, value) in attrs:
-			if key == 'href':
-			newURL = parse.urljoin(self.baseUrl, value)
-			self.links = self.links + [newUrl]
+def getTimes(query,num):
+    url = "http://query.nytimes.com/svc/cse/v2/sitesearch.json?query="+query.replace(" ","%20")+"&pt=article&page="+str(num)
+    jtext = urllib.urlopen(url)
+    return jtext
 
-	def getLinks(self, url):
-		self.links = []
-		self.baseURl = url
-		response = urlopen(url)
-			if response.getheader('Content-Type')=='text/html':
-				htmlBytes = response.read()
-				htmlString = htmlBytes.decode("utf-8")
-				self.feed(htmlString)
-				return htmlString, self.links
-			else:
-				return "",[]
-	def spider(url, word, maxPages):
-		pagesToVisit = [url]
-		numberVisited = 0
-		foundWord = False
-		
-		while numberVisted < maxPages and pagesToVisit =! [] and not foundWord:
-			numberVisited = numberVisted + 1
-			url = pagesToVisit[0]
-			pagesToVisit = pagesToVisit[1:]
-			try:
-				print(numberVisited, "Visiting:", url)
-				parser = LinkParser()
-				data, links = parser.getLinks(url)
-				data, links = parser.getLinks(url)
-					foundWord = True
-					pagesToVisit = pagesToVisit + links
-					print(" **Success!**")
-				except:
-					print(" **Failed!**")
-				if foundWord:
-					print("The word", word, "was found at", url)
-				else:
-					print("Word never found")
+
+
+
+def search(term):
+    page_number = 0
+    meta = 1
+    while meta > 0 and page_number<1:
+        resp = json.load(getTimes(term,page_number))
+        meta = int(resp['results']['meta']['payload'])
+        for res in resp['results']['results']:
+            print res['snippet']
+            #headline = res['hdl']
+            #snippet = res['snippet']
+            #author = res['cre']
+            #url1 = res['url']
+            #print headline
+            #print url
+        page_number+=1
+
+
+
+
+search("exotic cars")
